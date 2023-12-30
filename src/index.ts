5a623fcd72bea6368cdd1fc41b37991b26cc7d85
Vu Van Dung <me@joulev.dev>
2023-12-30 10:48:17 +0700

    now take user-provided fileContent data
    Signed-off-by: Vu Van Dung <me@joulev.dev>

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,6 @@ import { unlink } from "node:fs/promises";
 
 const allowedApiKeys = new Set((process.env.ALLOWED_API_KEYS || "").split(","));
 
-const content = `
-\\documentclass[tikz]{standalone}
-\\usetikzlibrary{fadings}
-\\begin{document}
-\\begin{tikzpicture}
-  \\draw[-latex] (-.5,0) node[left] {$0$} --(5,0) node[right] {$t$/s};
-  \\draw[-latex] (0,-.5) node[below] {$0$} --(0,3.5) node[left] {$A$/mm};
-  \\draw (.2,2.7)--(-.2,2.7) node[left] {$A_0$};
-  \\draw [very thick, red] (0,2.7) to [out=-45, in =170] (4.5,.5);
-\\end{tikzpicture}
-\\end{document}
-`.trim();
-
 // Workaround of Bun not doing multipart/form-data well
 async function getPdf(form: FormData) {
   const textBody = await new Response(form).text();
@@ -42,34 +29,38 @@ const app = new Elysia()
     }
   })
 
-  .get("/v1", async ({ set }) => {
-    const form = new FormData();
-    form.append("filename[]", "document.tex");
-    form.append("filecontents[]", content);
-    form.append("return", "pdf");
+  .post(
+    "/v1",
+    async ({ body, set }) => {
+      const form = new FormData();
+      form.append("filename[]", "document.tex");
+      form.append("filecontents[]", body.fileContent);
+      form.append("return", "pdf");
 
-    const pdfResponse = await getPdf(form);
+      const pdfResponse = await getPdf(form);
 
-    const pdfFileName = `/tmp/${crypto.randomUUID()}.pdf`;
-    const svgFileName = `/tmp/${crypto.randomUUID()}.svg`;
-    await Bun.write(pdfFileName, pdfResponse);
+      const pdfFileName = `/tmp/${crypto.randomUUID()}.pdf`;
+      const svgFileName = `/tmp/${crypto.randomUUID()}.svg`;
+      await Bun.write(pdfFileName, pdfResponse);
 
-    // Only capture the first page
-    const process = Bun.spawnSync(["pdf2svg", pdfFileName, svgFileName, "1"]);
+      // Only capture the first page
+      const process = Bun.spawnSync(["pdf2svg", pdfFileName, svgFileName, "1"]);
 
-    if (!process.success) {
-      set.status = 500;
-      return process.stderr;
-    }
+      if (!process.success) {
+        set.status = 500;
+        return process.stderr;
+      }
 
-    const svg = await Bun.file(svgFileName).text();
+      const svg = await Bun.file(svgFileName).text();
 
-    // Delete the temporary files
-    Promise.all([pdfFileName, svgFileName].map(unlink));
+      // Delete the temporary files
+      Promise.all([pdfFileName, svgFileName].map(unlink));
 
-    set.headers["Content-Type"] = "image/svg+xml";
-    return svg;
-  })
+      set.headers["Content-Type"] = "image/svg+xml";
+      return svg;
+    },
+    { body: t.Object({ fileContent: t.String() }) }
+  )
 
   .listen(process.env.PORT || 3000);
 
